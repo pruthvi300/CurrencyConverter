ff874154e751f78f8e7f65146fa8c7b131c60f26
Pruthviraj kakade <pruthvi3.pk@gmail.com>
2025-03-26 22:32:08 +0530

    issue fix

--- app/src/hooks/useCurrencyInfo.js
+++ app/src/hooks/useCurrencyInfo.js
@@ -7,7 +7,7 @@ function useCurrencyInfo(currency) {
     useEffect(()=>{
         fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
         .then((res)=> res.json())
-        .then((res)=> setData(res.currency) )
+        .then((res)=> setData(res[currency]) )
     },[currency])
 
     return data
