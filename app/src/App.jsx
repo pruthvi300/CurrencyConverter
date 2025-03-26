b61d8c2b044da458f782747988ee6a60f46ec380
Pruthviraj kakade <pruthvi3.pk@gmail.com>
2025-03-26 22:43:59 +0530

    bg change

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -29,7 +29,7 @@ function App() {
     <div
         className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
         style={{
-            backgroundImage: `url('https://images.pexels.com/photos/259209/pexels-photo-259209.jpeg?auto=compress&cs=tinysrgb&w=400')`,
+            backgroundImage: `url('https://images.pexels.com/photos/3532540/pexels-photo-3532540.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')`,
         }}
     >
         <div className="w-full">
